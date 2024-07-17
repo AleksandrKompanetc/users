e03fa6a0d3d0799033e72255d73df80d524e8a37
AleksandrKompanetc <moneymacker7@gmail.com>
2024-07-17 14:21:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import { Success } from './components/Success';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then((res) => res.json())
+    .then((json) => {
+      setUsers(json.data);
+    }).catch(err => {
+      console.warn(err);
+      alert('Error');
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <Success />
+    </div>
+  );
+}
+
+export default App;
